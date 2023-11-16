3814503f2a55dab96005c0613d6bd07dd4f15d41
Rebecca Barger <43703655+RebeccaLeeB@users.noreply.github.com>
2023-11-15 19:54:45 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+
+let audio = new Audio("tunes/a.wav"); //by default, audio src is "a" tune
+
+const playTune = (key) => {
+    audio.src = `tunes/${key}.wav`; //passing audio src based on key pressed
+    audio.play(); //playing audio
+
+    const clickKey = document.querySelector(`[data-key="${key}"]`); // getting cliked key elmement
+    clickedKey.classList.add('active');
+    setTimeout(() => {
+        clickedKey.classList.remove('active');
+    }, 150);
+
+}
+
+
+pianoKeys.forEach(key => {
+    //calling playTune function with passing data-key value as an argument.
+    key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+const pressedKey = (e) => {
+    playTune(e.key);
+}
+
+document.addEventListener("keydown", pressedKey);
